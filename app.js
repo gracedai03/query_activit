2f8e80ed962a1c245b61aca1427d735ff90d93dd
gmdai <grace.m.dai@gmail.com>
2025-04-10 15:36:55 -0500

    initialize

---
+++ app.js
@@ -0,0 +1,64 @@
+let teams = [
+  {
+    teamName: "Real Madrid",
+    city: "Madrid",
+    country: "Spain",
+    topScorers: ["Ronaldo", "Benzema", "Hazard"],
+    worldwideFansMillions: 798,
+  },
+  {
+    teamName: "Barcelona",
+    city: "Barcelona",
+    country: "Spain",
+    topScorers: ["Messi", "Suarez", "Puyol"],
+    worldwideFansMillions: 738,
+  },
+  {
+    teamName: "Manchester United",
+    city: "Manchester",
+    country: "England",
+    topScorers: ["Cantona", "Rooney", "Ronaldo"],
+    worldwideFansMillions: 755,
+  },
+  {
+    teamName: "Manchester City",
+    city: "Manchester",
+    country: "England",
+    topScorers: ["Sterling", "Aguero", "Haaland"],
+    worldwideFansMillions: 537,
+  },
+  {
+    teamName: "Brazil National Team",
+    city: "Not applicable",
+    country: "Brazil",
+    topScorers: ["Ronaldinho", "Cafu", "Bebeto"],
+    worldwideFansMillions: 950,
+  },
+  {
+    teamName: "Argentina National Team",
+    city: "Not applicable",
+    country: "Argentina",
+    topScorers: ["Messi", "Batistuta", "Maradona"],
+    worldwideFansMillions: 888,
+  },
+  {
+    teamName: "Atletico Madrid",
+    city: "Madrid",
+    country: "Spain",
+    topScorers: ["Aragonés", "Griezmann", "Torez"],
+    worldwideFansMillions: 40,
+  },
+];
+
+let t1 = {
+  teamName: "Real Madrid",
+  city: "Madrid",
+  country: "Spain",
+  topScorers: ["Ronaldo", "Benzema", "Hazard"],
+  worldwideFansMillions: 798,
+};
+
+// Add each team to the Firestore collection
+teams.forEach((team) => {
+  db.collection("teams").add(team);
+});
